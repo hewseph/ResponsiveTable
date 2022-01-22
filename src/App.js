66021cab05a2c9c18902a14cd551040dc71e1d75
Hew Keller <hew.keller@10federal.com>
2022-01-22 14:23:05 -0500

    example done.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
 import './App.css';
+import { data } from './data'
+import styled from 'styled-components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Table>
+      <Header>
+        <strong>ID</strong>
+      </Header>
+      <Header>
+        <strong>FIrst name</strong>
+      </Header>
+      <Header>
+        <strong>Last name</strong>
+      </Header>
+      <Header>
+        <strong>Email</strong>
+      </Header>
+      <Header>
+        <strong>Street</strong>
+      </Header>
+      <Header>
+        <strong>Country</strong>
+      </Header>
+      <Header>
+        <strong>University</strong>
+      </Header>
+      <Header>
+        <strong>IBAN</strong>
+      </Header>
+      {data.map((tr, r) => (<TableRow key={`row${r}`} tr={tr} />))}
+    </Table>
+  )
 }
+
+const TableRow = ({ tr }) => {
+  const [collapsed, setCollapsed] = useState(true)
+  return (
+    <>
+      {tr.map(td => (
+        <Cell onClick={() => setCollapsed(!collapsed)} key={td}>{td}</Cell>
+      ))}
+      <RowDetails duration={250} height={250} collapsed={collapsed}>
+        <Stuff>
+          DO STUFF HERE
+        </Stuff>
+      </RowDetails>
+    </>
+  )
+}
+
+const Header = styled.span`
+  background: #6c7ae0;
+  text-align: left;
+  font-weight: normal;
+  font-size: 1.1rem;
+  color: white;
+`
+
+const Table = styled.span({
+  overflow: "hidden",
+  overflowX: "scroll",
+  "::-webkit-scrollbar": {
+    width: "0px", /* Remove scrollbar space */
+    background: "transparent"  /* Optional: just make scrollbar invisible */
+  },
+  display: 'grid',
+  borderTop: "1px solid black",
+  borderRight: "1px solid black",
+  gridTemplateColumns: `
+    minmax(60px, 1fr)
+    minmax(100px, 1fr)
+    minmax(100px, 1fr)
+    minmax(200px, 1fr)
+    minmax(100px, 1fr)
+    minmax(100px, 1fr)
+    minmax(200px, 1fr)
+    minmax(200px, 1fr)
+    `
+})
+
+const Cell = styled.span`
+  padding: 8px 4px;
+  border-left: 1px solid black;
+  border-bottom: 1px solid black;
+  cursor: pointer;
+`
+
+const Stuff = styled.div({
+  padding: "100px"
+})
+
+const RowDetails = styled.span(props => ({
+  textAlign: "center",
+  gridColumn: "1/-1",
+  padding: "0",
+  borderLeft: "none",
+  borderBottom: "none",
+  height: "250px",
+  overflow: 'hidden',
+  transition: props.duration ? `max-height ${props.duration / 1000}s ease-in` : 'max-height:0.5s ease-in',
+  ...(props.collapsed ? {
+    borderLeft: "none",
+    borderBbottom: "none",
+    maxHeight: "0px",
+  } : {
+    borderLeft: "1px solid black",
+    borderBottom: "1px solid black",
+    maxHeight: props.height,
+  }),
+}))
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,102 @@
+export const headers = [
+  "ID <span class=\"resize-handle\"></span>",
+  "First name <span class=\"resize-handle\"></span>",
+  "Last name <span class=\"resize-handle\"></span>",
+  "Email <span class=\"resize-handle\"></span>",
+  "Street <span class=\"resize-handle\"></span>",
+  "Country <span class=\"resize-handle\"></span>",
+  "University <span class=\"resize-handle\"></span>",
+  "IBAN <span class=\"resize-handle\"></span>"
+]
+export const data = [
+  [
+    "000001",
+    "Lani",
+    "Ovendale",
+    "lovendale0@w3.org",
+    "7850 Old Shore Drive",
+    "United Kingdom",
+    "University of Plymouth",
+    "BG34 MPVP 8782 88EX H1CJ SC"
+  ],
+  [
+    "000002",
+    "Israel",
+    "Tassell",
+    "itassell1@ow.ly",
+    "245 Merchant Circle",
+    "Macedonia",
+    "South East European University",
+    "FR11 4824 2942 41H9 XBHC 46P2 O86"
+  ],
+  [
+    "000003",
+    "Eveleen",
+    "Mercer",
+    "emercer2@ow.ly",
+    "70700 Kipling Pass",
+    "Portugal",
+    "Instituto Superior Novas Profissões - INP",
+    "GR96 7559 456P GUAN WTAJ 3VPB S0P"
+  ],
+  [
+    "000004",
+    "Conn",
+    "Whitley",
+    "cwhitley3@wsj.com",
+    "03 Service Terrace",
+    "Albania",
+    "Epoka University",
+    "LI59 1813 2T7T VKTO 6RKE X"
+  ],
+  [
+    "000005",
+    "Cherye",
+    "Smitheram",
+    "csmitheram4@rambler.ru",
+    "9 Eliot Parkway",
+    "Indonesia",
+    "Universitas Mahasaraswati Denpasar",
+    "BR27 4570 4226 4255 5239 0197 316T J"
+  ],
+  [
+    "000006",
+    "Bunnie",
+    "Sked",
+    "bsked5@51.la",
+    "03418 Ludington Plaza",
+    "Nigeria",
+    "Federal University of Technology, Minna",
+    "ES45 6721 1332 3288 6455 1242"
+  ],
+  [
+    "000007",
+    "Helaine",
+    "Crother",
+    "hcrother6@opera.com",
+    "7932 Sloan Park",
+    "Philippines",
+    "Saint Ferdinand College",
+    "GB50 HFAD 8121 3729 9841 31"
+  ],
+  [
+    "000008",
+    "Tana",
+    "Ajean",
+    "tajean7@sfgate.com",
+    "2 Schurz Junction",
+    "China",
+    "Xi'an University of Electronic Science and Technology",
+    "KZ85 7422 XDPB P2VQ H8SR"
+  ],
+  [
+    "000009",
+    "Sollie",
+    "Greenrde",
+    "sgreenrde8@wikispaces.com",
+    "055 Mockingbird Way",
+    "Russia",
+    "St. Petersburg State Mining Institute (Technical University)",
+    "CH61 6423 9T4W WP0I 8MUK C"
+  ]
+]
